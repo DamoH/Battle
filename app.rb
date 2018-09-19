ca7469ed98c4c4ef2a4fe55faf22b9234298cd2e
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-09-19 17:47:17 +0100

    committing Battle pairing challenge

---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+
+  enable :sessions
+
+  get '/' do
+    erb(:index)
+  end
+
+  post '/names' do
+    session[:uno] = params[:player_one]
+    session[:deux] = params[:player_two]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one = session[:uno]
+    @player_two = session[:deux]
+    erb(:play)
+  end
+
+  get '/attack' do
+    @player_one = session[:uno]
+    @player_two = session[:deux]
+    erb :attack
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+
+end
